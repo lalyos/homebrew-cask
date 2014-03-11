f52f9c091200ccb75625caadf4454d407dfdd7ce
lalyos <lalyos@yhoo.com>
2014-03-11 19:21:20 +0100

    Adde github's atom editor from private s3:// url

---
+++ Casks/atom.rb
@@ -0,0 +1,7 @@
+class Atom < Cask
+  url 'https://s3-eu-west-1.amazonaws.com/sequenceiq/atom-mac.zip'
+  homepage 'https://atom.io/'
+  version '0.71.0'
+  sha256 'e393073bdcfb7a09225cf468c0d94c2ce9b3aab6ca6b40d6df79f079d837b15c'
+  link 'Atom.app' 
+end
