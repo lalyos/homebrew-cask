1cc7e187f0f48726557b6590c3ae07bea1646ede
lalyos <lalyos@yhoo.com>
2014-03-19 19:20:57 +0100

    atom 0.73.0

--- Casks/atom.rb
+++ Casks/atom.rb
@@ -1,7 +1,7 @@
 class Atom < Cask
   url 'https://s3-eu-west-1.amazonaws.com/sequenceiq/atom-mac.zip'
   homepage 'https://atom.io/'
-  version '0.71.0'
-  sha256 'e393073bdcfb7a09225cf468c0d94c2ce9b3aab6ca6b40d6df79f079d837b15c'
+  version '0.73.0'
+  sha256 '64aa93a8293d5e501977b17b9ac39ad11b4ad8f79d19811783ca42003325b90e'
   link 'Atom.app' 
 end
