9b6418eb2d56ec23ea739725188b631574834ff8
lalyos <lalyos@yhoo.com>
2014-04-09 08:32:26 +0200

    atom 0.84.0

--- Casks/atom.rb
+++ Casks/atom.rb
@@ -1,7 +1,7 @@
 class Atom < Cask
   url 'https://s3-eu-west-1.amazonaws.com/sequenceiq/atom-mac.zip'
   homepage 'https://atom.io/'
-  version '0.81.0'
-  sha256 '78e2168274fd2bf9943135bda8d808b8607c3f4607ea2d59f46dc29f009618c8'
+  version '0.84.0'
+  sha256 '9246de1258e35dd019af6268c454dd365ccfd01648fb418a99f3c90902b2ed55'
   link 'Atom.app' 
 end
