a04fc28669be6b5d5c5e622de138fd4c81e72673
lalyos <lalyos@yhoo.com>
2014-04-09 09:36:36 +0200

    s3 upload added to update script

--- update-atom.sh
+++ update-atom.sh
@@ -1,8 +1,29 @@
+#!/bin/bash
+
+ZIP=~/Downloads/atom-mac.zip
+if [ ! -f $ZIP ]; then
+  echo missing $ZIP
+  exit 1
+fi
+
 cd /usr/local/Library/Taps/phinze-cask
-unzip -qod /tmp/ ~/Downloads/atom-mac.zip
+rm -rf /tmp/Atom.app
+echo unzip $ZIP into /tmp ...
+unzip -qod /tmp/ $ZIP
 
 VERSION=$(cat /tmp/Atom.app/Contents/Resources/app/package.json|jq .version -r)
 SHA=$(shasum -a 256 ~/Downloads/atom-mac.zip|sed "s/[^0-9a-z].*//")
+echo $ZIP VERSION=$VERSION
+
+ACTUAL_VERSION=$(sed -n  "/version/ s/.*[^0-9]\([0-9\.]\{5,\}\).*/\1/p"  Casks/atom.rb)
+
+
+if [ "$VERSION" == "$ACTUAL_VERSION" ]; then
+  echo formula is uptodate: $VERSION
+  exit 1
+fi
+
+aws s3 cp ~/Downloads/atom-mac.zip s3://sequenceiq/
 
 sed -i "/version/ s/[0-9\.]\{5,\}/$VERSION/"  Casks/atom.rb
 sed -i "/sha256/ s/[a-z0-9]\{64\}/$SHA/" Casks/atom.rb
@@ -13,7 +34,6 @@ git fetch origin
 git rebase origin/master
 git push lalyos master --force
 
-rm ~/Downloads/atom-mac*.zip
-rm -rf /tmp/Atom.app
+#rm ~/Downloads/atom-mac*.zip
 cd -
 brew cask install atom
