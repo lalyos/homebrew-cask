28f51672b5c3d261329552e85d9a4320516bdee6
lalyos <lalyos@yahoo.com>
2014-06-07 15:05:12 +0200

    docker 0.12.0

--- Casks/docker.rb
+++ Casks/docker.rb
@@ -1,11 +1,11 @@
 class Docker < Cask
-  url 'http://get.docker.io/builds/Darwin/x86_64/docker-0.11.1'
+  url 'http://get.docker.io/builds/Darwin/x86_64/docker-0.12.0'
   homepage 'https://www.docker.io/'
-  version '0.11.1'
-  sha256 '386ffa26e52856107efb0b3075625d5b2331fa5acc8965fef87c1ab7d900c4e9'
-  binary 'docker-0.11.1', :target => '/usr/local/bin/docker'
+  version '0.12.0'
+  sha256 'a38dccb7f544fad4ef2f95243bef7e2c9afbd76de0e4547b61b27698bf9065f3'
+  binary 'docker-0.12.0', :target => '/usr/local/bin/docker'
   container_type 'naked'
   after_install do
-    system "chmod", "755", "#{destination_path}/docker-0.11.1"
+    system "chmod", "755", "#{destination_path}/docker-0.12.0"
   end
 end
