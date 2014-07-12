fce3b22f2848aec065a1e87a897ef2d837750144
lalyos <lalyos@yahoo.com>
2014-07-12 15:18:07 +0200

    Upgrade docker to 1.1.1

--- Casks/docker.rb
+++ Casks/docker.rb
@@ -1,11 +1,11 @@
 class Docker < Cask
-  url 'http://get.docker.io/builds/Darwin/x86_64/docker-1.1.0'
+  url 'http://get.docker.io/builds/Darwin/x86_64/docker-1.1.1'
   homepage 'https://www.docker.io/'
-  version '1.1.0'
-  sha256 '4905fa4d141fbffab7721be4f8a3e8fd788e6f377b92d29d91e85c8de97d7887'
-  binary 'docker-1.1.0', :target => '/usr/local/bin/docker'
+  version '1.1.1'
+  sha256 'bbe72c00757bb4de96edbc7d69b329b7ea4fc87edd4a53655f7048cdde95ca04'
+  binary 'docker-1.1.1', :target => '/usr/local/bin/docker'
   container_type 'naked'
   after_install do
-    system "chmod", "755", "#{destination_path}/docker-1.1.0"
+    system "chmod", "755", "#{destination_path}/docker-1.1.1"
   end
 end
